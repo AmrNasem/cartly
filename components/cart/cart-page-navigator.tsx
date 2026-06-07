462a6abfe991f5a693a17b9954570f01333c7e4a
Amr Naseem <amr.nasem51@gmail.com>
2026-06-07 22:31:02 +0300

    fix(cart): unexpected 0 on cart icon in navbar when cart is empty

--- components/cart/cart-page-navigator.tsx
+++ components/cart/cart-page-navigator.tsx
@@ -8,7 +8,7 @@ function CartPageNavigator() {
   const cartItems = useCartStore(state => state.cart.items);
   return (
     <Link href="/cart" className="relative hover:bg-[#e9ebef] p-1 rounded-md duration-150 cursor-pointer">
-      {cartItems?.length &&
+      {!!cartItems?.length &&
         <span className="absolute bottom-full end-full translate-3 h-5 min-w-5 flex items-center justify-center text-white bg-primary/80 rounded-full p-1 text-[12px] font-semibold">{cartItems.length}</span>
       }
       <ShoppingCart className="size-4 text-foreground" />
