a0fe4a42ad026020a608776619c88f76085702de
Amr Naseem <amr.nasem51@gmail.com>
2026-07-02 01:10:31 +0300

    fix(typecheck): inferred session type instead of creating one

--- lib/auth/session.ts
+++ lib/auth/session.ts
@@ -6,3 +6,7 @@ export async function getSession() {
     headers: await headers(),
   });
 }
+
+// export type Session = typeof getSession extends () => Promise<infer U> ? U : never;
+export type Session = Awaited<ReturnType<typeof getSession>>
+export type AuthUser = NonNullable<Session>["user"];
